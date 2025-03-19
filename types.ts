3cada412bab35f49513c636143e8b0282a38b9fc
celmaun <salman@osalman.com>
2025-03-19 08:06:22 +0500

    Auto-format

--- types.ts
+++ types.ts
@@ -1,116 +1,45 @@
-export type i8 = (Number & { readonly ["#Int8"]: unique symbol }) | 0 | -1 | 1;
-export type i16 =
-  | (Number & { readonly ["#Int16"]: unique symbol })
-  | 0
-  | -1
-  | 1
-  | 16;
-export type i32 =
-  | (Number & { readonly ["#Int32"]: unique symbol })
-  | 0
-  | -1
-  | 1
-  | 32;
+export type i8 = (Number & { readonly ['#Int8']: unique symbol }) | 0 | -1 | 1;
+export type i16 = (Number & { readonly ['#Int16']: unique symbol }) | 0 | -1 | 1 | 16;
+export type i32 = (Number & { readonly ['#Int32']: unique symbol }) | 0 | -1 | 1 | 32;
 // export type Int32 = i32;
 
-export type u8 =
-  | (Number & { readonly ["#Uint8"]: unique symbol })
-  | 0
-  | 1
-  | 8
-  | 255;
-export type u16 =
-  | (Number & { readonly ["#Uint16"]: unique symbol })
-  | 0
-  | 1
-  | 16
-  | 65535;
-export type u32 =
-  | (Number & { readonly ["#Uint32"]: unique symbol })
-  | 0
-  | 1
-  | 32
-  | 4294967295;
+export type u8 = (Number & { readonly ['#Uint8']: unique symbol }) | 0 | 1 | 8 | 255;
+export type u16 = (Number & { readonly ['#Uint16']: unique symbol }) | 0 | 1 | 16 | 65535;
+export type u32 = (Number & { readonly ['#Uint32']: unique symbol }) | 0 | 1 | 32 | 4294967295;
 // export type Uint32 = u32;
 
-export type f32 = Number & { readonly ["#Float32"]: unique symbol };
+export type f32 = Number & { readonly ['#Float32']: unique symbol };
 // export type Float32 = f32;
-export type f64 = Number & { readonly ["#Float64"]: unique symbol };
+export type f64 = Number & { readonly ['#Float64']: unique symbol };
 // export type Float64 = f64;
 
 export type NumberCastable = number | i32 | u32 | string | boolean;
 
-export type BigIntCastable =
-  | boolean
-  | number
-  | bigint
-  | i8
-  | u8
-  | i16
-  | u16
-  | i32
-  | u32
-  | i64
-  | u64
-  | string;
+export type BigIntCastable = boolean | number | bigint | i8 | u8 | i16 | u16 | i32 | u32 | i64 | u64 | string;
 
-export type i64 =
-  | (BigInt & { readonly ["#BigInt64"]: unique symbol })
-  | 0n
-  | -1n
-  | 1n
-  | 64n;
+export type i64 = (BigInt & { readonly ['#BigInt64']: unique symbol }) | 0n | -1n | 1n | 64n;
 
 // export type BigInt64 = i64;
 
-export type u64 =
-  | (BigInt & { readonly ["#BigUint64"]: unique symbol })
-  | 0n
-  | 1n;
+export type u64 = (BigInt & { readonly ['#BigUint64']: unique symbol }) | 0n | 1n;
 
 // export type BigUint64 = u64;
 
-export type Numeric =
-  | number
-  | bigint
-  | i8
-  | u8
-  | i16
-  | u16
-  | i32
-  | u32
-  | i64
-  | u64
-  | f32
-  | f64
-  | i64
-  | u64;
+export type Numeric = number | bigint | i8 | u8 | i16 | u16 | i32 | u32 | i64 | u64 | f32 | f64 | i64 | u64;
 
 export type IntType = i8 | u8 | i16 | u16 | i32 | u32 | i64 | u64;
 
 export type UintType = u8 | u16 | u32 | u64;
 
-export type ArithmeticInfixOperator = "+" | "-" | "*" | "/" | "%" | "%%" | "**";
-export type ArithmeticUnaryOperator = "-" | "+";
-export type BitwiseInfixOperator = "&" | "|" | "^" | "<<" | ">>" | ">>>";
-export type BitwiseUnaryOperator = "~";
+export type ArithmeticInfixOperator = '+' | '-' | '*' | '/' | '%' | '%%' | '**';
+export type ArithmeticUnaryOperator = '-' | '+';
+export type BitwiseInfixOperator = '&' | '|' | '^' | '<<' | '>>' | '>>>';
+export type BitwiseUnaryOperator = '~';
 
-export type NumericComparisonOperator =
-  | "=="
-  | "!="
-  | "<"
-  | ">"
-  | "<="
-  | ">="
-  | "==="
-  | "!==";
+export type NumericComparisonOperator = '==' | '!=' | '<' | '>' | '<=' | '>=' | '===' | '!==';
 
-export type NumericInfixOperator =
-  | ArithmeticInfixOperator
-  | BitwiseInfixOperator;
-export type NumericUnaryOperator =
-  | ArithmeticUnaryOperator
-  | BitwiseUnaryOperator;
+export type NumericInfixOperator = ArithmeticInfixOperator | BitwiseInfixOperator;
+export type NumericUnaryOperator = ArithmeticUnaryOperator | BitwiseUnaryOperator;
 
 export type SpacedNumericComparisonOperator = ` ${NumericComparisonOperator} `;
 export type SpacedNumericUnaryOperator = ` ${NumericUnaryOperator} `;
@@ -149,10 +78,7 @@ export interface BitwiseMath<T> {
   shr(value: T, shift: T): T; // >>
 }
 
-export interface IntMath<T>
-  extends ArithmeticMath<T>,
-    BitwiseMath<T>,
-    CompareMath<Numeric> {
+export interface IntMath<T> extends ArithmeticMath<T>, BitwiseMath<T>, CompareMath<Numeric> {
   // Performs the sign-agnostic count leading zero bits operation on a 32-bit or 64-bit integer. All zero bits are considered leading if the value is zero.
   clz(value: T): T;
 
@@ -178,13 +104,9 @@ export interface UintMath<T> extends IntMath<T> {
 
 export type I64_Safe = i8 | u8 | i16 | u16 | i32 | u32 | i64;
 
-export type I64_TemplateUnary =
-  | TemplateStringsArray
-  | [NumericUnaryOperator, ""];
+export type I64_TemplateUnary = TemplateStringsArray | [NumericUnaryOperator, ''];
 
-export type I64_TemplateInfix =
-  | TemplateStringsArray
-  | ["", `${" " | ""}${NumericInfixOperator}${"" | " "}`, ""];
+export type I64_TemplateInfix = TemplateStringsArray | ['', `${' ' | ''}${NumericInfixOperator}${'' | ' '}`, ''];
 
 export interface i64Constructor extends IntMath<i64> {
   readonly MIN_VALUE: -9223372036854775808n;
@@ -198,11 +120,7 @@ export interface i64Constructor extends IntMath<i64> {
 
   tag(arg0: TemplateStringsArray): i64;
   tag(arg0: I64_TemplateUnary, arg1?: BigIntCastable): i64;
-  tag(
-    tsa: I64_TemplateInfix,
-    arg1?: BigIntCastable,
-    arg2?: BigIntCastable
-  ): i64;
+  tag(tsa: I64_TemplateInfix, arg1?: BigIntCastable, arg2?: BigIntCastable): i64;
 
   readonly prototype: i64;
   is(value: unknown): value is i64;
